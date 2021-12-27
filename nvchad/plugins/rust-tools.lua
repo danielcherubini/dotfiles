57492cab4e5e1830d352e2b4e248f7bf08ae947a
Daniel Cherubini <daniel@cherubini.casa>
2021-12-28 00:33:05 +0100

    fuxing issue

--- nvchad/plugins/rust-tools.lua
+++ nvchad/plugins/rust-tools.lua
@@ -5,17 +5,13 @@ local M = {}
 M.setup = function()
   rust_tools.setup {
     tools = {
-        autoSetHints = true,
-        hover_with_actions = true,
-        inlay_hints = {
-            show_parameter_hints = false,
-            parameter_hints_prefix = "",
-            other_hints_prefix = "",
-        },
-    },
+      hover_actions = {
+        auto_focus = true,
+      },
     -- all the opts to send to nvim-lspconfig
     -- these override the defaults set by rust-tools.nvim
     -- see https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#rust_analyzer
+    },
     server = {
         -- on_attach is a callback called when the language server attachs to the buffer
         on_attach = function(client)
@@ -27,7 +23,6 @@ M.setup = function()
             -- to enable rust-analyzer settings visit:
             -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
             ["rust-analyzer"] = {
-                -- enable clippy on save
                 checkOnSave = {
                     command = "clippy"
                 },
