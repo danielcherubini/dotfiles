0c46b5ec8ec77fa4b1a0d131c02c6b572c5fd156
Daniel Cherubini <daniel@cherubini.casa>
2022-01-05 05:54:09 +0100

    moving to lsp-installer

--- nvchad/plugins/lspconfig.lua
+++ nvchad/plugins/lspconfig.lua
@@ -1,14 +1,41 @@
 local M = {}
 
 M.setup_lsp = function(attach, capabilities)
-  local lspconfig = require "lspconfig"
+   local lsp_installer = require "nvim-lsp-installer"
 
-  lspconfig.gopls.setup {
-    on_attach = function(client)
-      client.resolved_capabilities.document_formatting = false
-      client.resolved_capabilities.document_range_formatting = false
-    end,
-  }
+   lsp_installer.on_server_ready(function(server)
+      local opts = {
+         on_attach = function(client, bufnr)
+           client.resolved_capabilities.document_formatting = false
+           client.resolved_capabilities.document_range_formatting = false
+           attach(client, bufnr)
+         end,
+         capabilities = capabilities,
+         flags = {
+            debounce_text_changes = 150,
+         },
+         settings = {},
+      }
+
+      if server.name == "rust_analyzer" then
+        require("rust-tools").setup {
+          server = vim.tbl_deep_extend("force", server:get_default_options(), opts, {
+            ["rust-analyzer"] = {
+              experimental = {
+                procAttrMacros = true,
+              },
+              checkOnSave = {
+                command = "clippy",
+              },
+            },
+          }),
+        }
+        server:attach_buffers()
+      else
+        server:setup(opts)
+        vim.cmd [[ do User LspAttachBuffers ]]
+      end
+   end)
 end
 
 return M
