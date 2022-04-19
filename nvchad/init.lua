4191b1b365d40c66a8a6605aa735e0240e4bcddc
Daniel Cherubini <daniel@cherubini.casa>
2022-04-19 18:17:29 +0200

    removing last commit

--- nvchad/init.lua
+++ nvchad/init.lua
@@ -2,14 +2,3 @@ local map = require("core.utils").map
 
  map("n", "<leader>lg", ":LazyGit<CR>")
  map("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
-
--- WSL yank support
-vim.cmd([[
-  let s:clip = '/mnt/c/Windows/System32/clip.exe'
-  if executable(s:clip)
-      augroup WSLYank
-          autocmd!
-          autocmd TextYankPost * if v:event.operator ==# 'y' | call system(s:clip, @0) | endif
-      augroup END
-  endif
-]])
