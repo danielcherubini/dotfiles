6d052b4a5dc6d9008fc79dc9a8a235985a149522
Daniel Cherubini <daniel@cherubini.casa>
2022-10-12 14:22:54 +0200

    stuff

--- nvchad/plugins/lspconfig.lua
+++ nvchad/plugins/lspconfig.lua
@@ -8,8 +8,8 @@ local servers = plugin_conf.lspconfig
 for _, server in ipairs(servers) do
   local serverOpts = {
     on_attach = function(client, bufnr)
-      client.resolved_capabilities.document_formatting = false
-      client.resolved_capabilities.document_range_formatting = false
+      client.server_capabilities.documentFormattingProvider = false
+      client.server_capabilities.documentRangeFormattingProvider = false
       on_attach(client, bufnr)
     end,
     capabilities = capabilities,
