2f99c06f8d01e47e036e1b7335a2e629caca6e00
Daniel Cherubini <daniel@cherubini.casa>
2024-05-01 21:05:34 +0200

    small changes

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -4,3 +4,21 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+-- vim.opt.clipboard = "unnamedplus"
+--
+-- if vim.fn.has "wsl" == 1 then
+--   vim.cmd [[
+--                 let g:clipboard = {
+--                 \   'name': 'WslClipboard',
+--                 \   'copy': {
+--                 \      '+': 'clip.exe',
+--                 \      '*': 'clip.exe',
+--                 \    },
+--                 \   'paste': {
+--                 \      '+': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+--                 \      '*': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+--                 \   },
+--                 \   'cache_enabled': 0,
+--                 \ }
+--         ]]
+-- end
