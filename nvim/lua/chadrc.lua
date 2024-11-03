51fe826ea713582826a3f82606da0c7f8c8db3ad
Daniel Cherubini <daniel@cherubini.casa>
2024-11-03 16:33:39 +0100

    adds mason

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -5,6 +5,8 @@ M.base46 = {
   hl_override = require "configs.highlights",
 }
 
+M.mason = { cmd = true, pkgs = require "configs.overrides".mason.pkgs }
+
 -- M.plugins = "custom.plugins"
 -- M.mappings = require "custom.mappings"
 
