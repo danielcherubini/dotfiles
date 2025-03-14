f71b06ec1b53e6186e5662aabf2922f53939ed4d
Daniel Cherubini <daniel@cherubini.casa>
2025-03-14 08:42:47 +0100

    updating gemini model

--- nvim/lua/configs/geminicode.lua
+++ nvim/lua/configs/geminicode.lua
@@ -3,7 +3,7 @@ M = {}
 M.config = function()
   require("gemini").setup {
     model_config = {
-      model_id = "gemini-2.0-flash-thinking-exp-01-21",
+      model_id = "gemini-2.0-flash-thinking-exp-02-05",
     },
   }
 end
