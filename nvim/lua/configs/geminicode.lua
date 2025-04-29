41f8a7b50621373e59720ba74c8a0a46483877e3
Daniel Cherubini <daniel@cherubini.casa>
2025-04-29 10:46:41 +0200

    updating the gemini plugin

--- nvim/lua/configs/geminicode.lua
+++ nvim/lua/configs/geminicode.lua
@@ -1,11 +1,7 @@
 M = {}
 
 M.config = function()
-  require("gemini").setup {
-    model_config = {
-      model_id = "gemini-2.0-flash-thinking-exp-02-05",
-    },
-  }
+  require("gemini").setup {}
 end
 
 M.keys = {
