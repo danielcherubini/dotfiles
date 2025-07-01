b4f9bf902a658a4cf725ec08106e95bf2a736c77
Daniel Cherubini <daniel@cherubini.casa>
2025-07-01 22:23:24 +0200

    ok

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -13,6 +13,27 @@ map("n", "<C-t>", function()
   require("menu").open "default"
 end, {})
 
+-- Terminal
+map({ "n", "t" }, "<A-v>", function()
+  require("nvchad.term").toggle {
+    pos = "bo vsp",
+    id = "vertTerm",
+    size = 0.5,
+  }
+end, { desc = "terminal toggle vertical term" })
+map({ "n", "t" }, "<A-i>", function()
+  require("nvchad.term").toggle {
+    pos = "float",
+    id = "floatTerm",
+    float_opts = {
+      row = 0.03,
+      col = 0.03,
+      width = 0.9,
+      height = 0.9,
+    },
+  }
+end, { desc = "terminal toggle floating term" })
+--
 -- mouse users + nvimtree users!
 map("n", "<RightMouse>", function()
   vim.cmd.exec '"normal! \\<RightMouse>"'
